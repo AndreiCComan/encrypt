c8fc9f61e91e7d37887b4b7d5fdb190e041a8807
AndreiCComan <andreicatalin.coman@studenti.unitn.it>
2017-01-14 12:11:56 +0100

    add readme, loading, links and other small things

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # encrypt
+
+## Demo
+### Here you can find a demo on heroku: https://goo.gl/6WnPNf
+
+## Description
+#### encrypt is a simple website that allows you to encrypt text. It is based on crypto-js library that you can find here: https://github.com/brix/crypto-js
+
+### encryptions supported
+* SHA-1
+* AES
+### decryptions supported
+* AES
--- js/script.js
+++ js/script.js
@@ -6,6 +6,22 @@ $(document).ready(function () {
         showContentPage();
     });
 
+    //Check scoll position in order to change navbar background color
+    if ($('.navbar').length > 0) {
+        $(window).on("scroll load resize", function () {
+            checkScroll();
+        });
+    }
+
+    //Navbar background switch
+    function checkScroll() {
+        var startY = $('.navbar').height() * 0.01;
+        if ($(window).scrollTop() > startY) {
+            $('.navbar').addClass("scrolled");
+        } else {
+            $('.navbar').removeClass("scrolled");
+        }
+    }
     //Activate navbar toggle and tooltip
     activateToggle();
     activateTooltip();
@@ -14,26 +30,48 @@ $(document).ready(function () {
     load_SHA1_listeners();
     load_AES_listeners();
     load_copyClipboar_listener();
-    
-    $("#en-sha1-link").on("click", function(){
+
+    $("#en-sha1-link").on("click", function () {
+        $(".alert-wrap").hide();
+        $(".link-active").attr("class", "link");
+        $(this).attr("class", "link-active");
+        $("#sha1-text").val("");
+        $("#result-section").hide();
         $("#sha1-section").show();
         $("#aes-section").hide();
     });
-    
-    $("#en-aes-link").on("click", function(){
+
+    $("#en-aes-link").on("click", function () {
+        $(".alert-wrap").hide();
+        $(".link-active").attr("class", "link");
+        $(this).attr("class", "link-active");
+        $("#aes-text").val("");
+        $("#aes-passphrase").val("");
+        $("#result-section").hide();
         $("#aes-section").show();
         $("#sha1-section").hide();
         $("#aes-section").attr("data-mode", "encrypt");
-        $("#aes-mode").text("encryption");
+        $("#aes-mode").text("en");
     });
-    
-    $("#de-aes-link").on("click", function(){
+
+    $("#de-aes-link").on("click", function () {
+        $(".alert-wrap").hide();
+        $(".link-active").attr("class", "link");
+        $(this).attr("class", "link-active");
+        $("#aes-text").val("");
+        $("#aes-passphrase").val("");
+        $("#result-section").hide();
         $("#aes-section").show();
         $("#sha1-section").hide();
         $("#aes-section").attr("data-mode", "decrypt");
-        $("#aes-mode").text("decryption");
+        $("#aes-mode").text("de");
     });
-    
+
+    //Hide navbar on link clicked
+    $('.navbar a').on('click', function (event) {
+        $("#theNavbar").collapse("hide");
+    });
+
 });
 
 //FUNCTIONS FOR THE SCRIPT
@@ -74,35 +112,36 @@ function load_SHA1_listeners() {
     var resultSection = $("#result-section");
     text.on("keyup", function () {
         if (text.val() != "") {
-            message.text("");
             encryptSHA1(text.val());
         } else {
             resultSection.hide();
-            message.text("Please fullfill everything.");
+            message.text("The textfield seems to be empty");
             alertWrap.show();
         }
     });
 }
 
 function encryptSHA1(text) {
-    console.log("Original Text = " + text);
     text = encodeURIComponent(text);
-    console.log("Encoded Text = " + encodeURI(text));
     var alertWrap = $(".alert-wrap");
     var message = $(".message");
     var resultSection = $("#result-section");
     var resultValue = $(".result-value");
+    var loadingSpinner = $("#loading-spinner-section");
+    loadingSpinner.show()
     $.ajax({
         url: '/encryptSHA1?',
         type: "POST",
         data: "text=" + text,
         dataType: "text",
         success: function (data) {
+            loadingSpinner.hide();
             alertWrap.hide();
             resultSection.show();
             resultValue.text(data);
         },
         error: function (data) {
+            loadingSpinner.hide();
             alertWrap.show();
             message.text("Something went wrong, please reload the page");
         }
@@ -117,7 +156,6 @@ function load_AES_listeners() {
     var resultSection = $("#result-section");
     $("#aes-text, #aes-passphrase").on("keyup", function () {
         if (text.val() != "" && passphrase.val() != "") {
-            message.text("");
             encryptAES(text.val(), passphrase.val());
         } else {
             resultSection.hide();
@@ -136,26 +174,35 @@ function encryptAES(text, passphrase) {
     var resultSection = $("#result-section");
     var resultValue = $(".result-value");
     var mode = $("#aes-section").attr("data-mode");
+    var loadingSpinner = $("#loading-spinner-section");
     var askToURL = "";
-    switch(mode){
-        case "encrypt": askToURL="/encryptAES?"; break;
-        case "decrypt": askToURL="/decryptAES?"; break;
-        default: askToURL = "";
+    switch (mode) {
+        case "encrypt":
+            askToURL = "/encryptAES?";
+            break;
+        case "decrypt":
+            askToURL = "/decryptAES?";
+            break;
+        default:
+            askToURL = "";
     }
+    loadingSpinner.show();
     $.ajax({
         url: askToURL,
         type: "POST",
         data: "text=" + text + "&passphrase=" + passphrase,
         dataType: "text",
         success: function (data) {
+            loadingSpinner.hide();
             alertWrap.hide();
             resultSection.show();
             resultValue.text(data);
         },
         error: function (data) {
+            loadingSpinner.hide();
             alertWrap.show();
             message.text("Something went wrong, please reload the page");
         }
     });
-    
+
 }
